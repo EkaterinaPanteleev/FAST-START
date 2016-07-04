34d9ecce58cb9195c5dd2eef2866811ea48c625e
Panteleev Ecaterina <panteleev.ekaterina@gmail.com>
2016-07-04 15:54:08 +0300

    less structure added

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,6 +176,22 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+
+    devUpdate: {
+      main: {
+        options: {
+          updateType: 'report', //just report outdated packages
+          reportUpdated: false, //don't report up-to-date packages
+          semver: true, //stay within semver when updating
+          packages: {
+              devDependencies: true, //only check for devDependencies
+              dependencies: false
+          },
+          packageJson: null, //use matchdep default findup to locate package.json
+          reportOnlyPkgs: [] //use updateType action on all packages
+        }
+      }
     }
 
   });
@@ -211,6 +227,10 @@ module.exports = function(grunt) {
     'img',
     'includereplace:html',
     'gh-pages',
+  ]);
+
+  grunt.registerTask('update', [
+    'devUpdate:main',
   ]);
 
 };
